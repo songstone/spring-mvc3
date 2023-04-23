5e9b44c95195669827a468400353af00058384d5
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-04-24 06:19:54 +0900

    test: single check box

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,6 +3,8 @@ package hello.itemservice.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter @Setter
 public class Item {
 
@@ -10,6 +12,11 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open;
+    private List<String> regions;
+    private ItemType itemType;
+    private String deliveryCode;
 
     public Item() {
     }
